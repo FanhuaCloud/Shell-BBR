1d43c2e50b708de9b47d4807a8b94fc23957a28f
fhcloud <1157173310@qq.com>
2017-09-02 22:26:44 +0800

    修改代码逻辑，增加优化内核参数功能

--- BBR_Install.sh
+++ BBR_Install.sh
@@ -106,10 +106,11 @@ function Installbasesoftware() {
 
 function Askuser() {
   OPTION=$(whiptail --clear --nocancel --menu "请选择选项" 15 60 4 \
-"1" "升级系统内核" \
+"1" "升级内核" \
 "2" "安装原版BBR(请确认已升级内核)" \
 "3" "安装魔改版BBR(请确认已升级内核)" \
-"4" "取消安装" \
+"4" "优化内核参数" \
+"5" "取消安装" \
 3>&1 1>&2 2>&3)
   Logprefix;echo ${CMSG}'[Info]提示:按下回车键开始，或使用CTRL+C退出'${CEND}
   read
@@ -124,14 +125,50 @@ function Askuser() {
     InstallBBR_tsunami
     ;;
   4)
+    Kernel_Optimize
+    ;;
+  5)
     exit
     ;;
   esac
 }
 
-function Installok() {
-  #安装完成提示
-  Logprefix;echo ${CMAGENTA}'[Success]安装完成'${CEND}
+function Kernel_Optimize() {
+  Logprefix;echo ${CMSG}'[Info]优化内核参数'${CEND}
+  echo 'net.ipv4.ip_forward = 0
+net.ipv4.conf.default.rp_filter = 1
+net.ipv4.conf.default.accept_source_route = 0
+kernel.sysrq = 0
+kernel.core_uses_pid = 1
+net.ipv4.tcp_syncookies = 1
+kernel.msgmnb = 65536
+kernel.msgmax = 65536
+kernel.shmmax = 68719476736
+kernel.shmall = 4294967296
+net.ipv4.tcp_max_tw_buckets = 6000
+net.ipv4.tcp_sack = 1
+net.ipv4.tcp_window_scaling = 1
+net.ipv4.tcp_wmem = 8192 4336600 873200
+net.ipv4.tcp_rmem = 32768 4336600 873200
+net.core.wmem_default = 8388608
+net.core.rmem_default = 8388608
+net.core.rmem_max = 16777216
+net.core.wmem_max = 16777216
+net.core.netdev_max_backlog = 262144
+net.core.somaxconn = 262144
+net.ipv4.tcp_max_orphans = 3276800
+net.ipv4.tcp_max_syn_backlog = 262144
+net.ipv4.tcp_timestamps = 1
+net.ipv4.tcp_synack_retries = 1
+net.ipv4.tcp_syn_retries = 1
+net.ipv4.tcp_tw_recycle = 1
+net.ipv4.tcp_tw_reuse = 1
+net.ipv4.tcp_mem = 786432 1048576 1572864
+net.ipv4.tcp_fin_timeout = 30
+#net.ipv4.tcp_keepalive_time = 30
+net.ipv4.tcp_keepalive_time = 300
+net.ipv4.ip_local_port_range = 1024 65000' > /etc/sysctl.conf
+Logprefix;echo ${CMAGENTA}'[Success]优化完成'${CEND}
 }
 
 function InstallKernel() {
@@ -181,4 +218,6 @@ Installbasesoftware
 Yumupdate
 
 #安装开始
-Askuser
+while true; do
+  Askuser
+done
