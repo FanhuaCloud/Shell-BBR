13149f03654b46c5e718c72d14aa9d6707a599ac
何凯皓 <1157173310@qq.com>
2018-05-05 09:31:03 +0800

    修改一处，现在安装完会直接覆盖原来的内核参数

--- BBR_Install_auto.sh
+++ BBR_Install_auto.sh
@@ -167,7 +167,7 @@ function InstallKernel() {
 }
 
 function InstallBBR() {
-  echo "net.core.default_qdisc = fq" >> /etc/sysctl.conf
+  echo "net.core.default_qdisc = fq" > /etc/sysctl.conf
   echo "net.ipv4.tcp_congestion_control = bbr" >> /etc/sysctl.conf
   sysctl -p
   lsmod | grep bbr
